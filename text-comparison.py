d5391ff987cb1f1789f32911ea18d2e474cf11a1
Kevin Rocci <kevinrocci@gmail.com>
2024-06-05 15:26:18 -0700

    Compare character count in ransom to magazine

--- text-comparison.py
+++ text-comparison.py
@@ -1,7 +1,9 @@
 # Prompt user for the first file name and handle errors
+
+print("\nWelcome. Let's determine if the ransom note was created with this magazine.\n")
+
 while True :
     first_fhandle = input('Enter the file name of the ransom note: ')
-    if len(first_fhandle) < 1 : first_fhandle = "ransom.txt"
     try:
         open_ransom_note = open(first_fhandle)
         break
@@ -11,7 +13,6 @@ while True :
 # Prompt user for the second file name and handle errors
 while True :
     second_fhandle = input('Enter the file name for the magazine: ')
-    if len(second_fhandle) < 1 : second_fhandle = "magazine.txt"
     try:
         open_magazine = open(second_fhandle)
         break
@@ -42,6 +43,10 @@ for lines in open_magazine :
             letter = letter.lower()
             magazine_hash[letter] = magazine_hash.get(letter, 0) + 1
 
-# print('First file: ', ransom_note_hash)
-# print('Second file: ', magazine_hash)
-
+# Compare values in rasome note to values in magazine
+for key in ransom_note_hash :
+    # print("Character: ", key, "Ransom note: ", ransom_note_hash[key], "Magazine :", magazine_hash[key])
+    if key in magazine_hash and ransom_note_hash[key] > magazine_hash[key] :
+        print("False: the ransom note was not created with this magazine.\n", key, "appears in the ransom note", ransom_note_hash[key], "times, and only appears in the magazine", magazine_hash[key], "times.")
+        break
+    print('True: the ransom note could have been created with this magazine.')
